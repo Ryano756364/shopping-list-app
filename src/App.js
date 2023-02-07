0fdd8438a38b2428f0e92daef97bcebc21e87777
Ryan O'Dell <110799654+Ryano756364@users.noreply.github.com>
2023-02-07 16:10:01 -0500

    Form setup with styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import {useState} from 'react';
 import './App.css';
+import ShopCreate from './components/ShopCreate';
 
 function App(){
+  const [shop, setShop] = useState([]); //Setting up empty array so we can build shopping list
+
+  const onCreateShop = (item) => {
+    console.log('Add item with name: ', item);
+  };
+  
   return (
     <div>
-      Main app component
+      <ShopCreate onCreate={onCreateShop}/>
     </div>
   )
 }
